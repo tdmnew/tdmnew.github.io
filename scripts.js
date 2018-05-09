282c2fc7ff1d2400e4385f3402650a347f6016d9
TN <TN@users.noreply.github.com>
2018-05-09 20:50:31 +0100

    Animation Works and Page Content Added
    No more animation issues, page content has been added.

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@ $(".header").hide(0)
 $(".navTop").hide(0);
 
 var navItems = $("#about, #blog, #projects");
-var navClicked = false; //determines whether .navTop should be displayed
+var firstAnimationPlayed = false; //determines whether .navTop should be displayed
 var navAnimationPlayed = false;
 
-var activeIcon;
+var activeTab;
 
 // On load, play animation (If using desktop)
 $(document).ready(function(){
@@ -25,7 +25,7 @@ $(window).resize(function(){
   if (size >= 746) {
     $(".navText").css({"fontSize": "2em"});
     navItems.css({"height": "75%"});
-    if(navClicked == false){
+    if(firstAnimationPlayed == false){
       navItems.css({"width": "25%"});
     } else {
       $(".navTop").fadeIn(1000);
@@ -40,59 +40,102 @@ $(window).resize(function(){
 
 
 $("#about, #blog, #projects, #topAbout, #topBlog, #topProjects").click(function( event ){
-  var clickedIcon = $(this).attr('id');
+  var clickedIcon = "#" + $(this).attr('id');
   var otherText;
   var otherIcons;
 
+  // Prevent clicks on already active tab
+  if (clickedIcon === activeTab) {
+    return;
+  }
+  //Prevent clicks until animation completes
+  if ( $(':animated' ).length) {
+    return false;
+  }
+
   // unbind event handler for intro nav items once clicked.
-  if (navClicked === false) {
-    navClicked = true;
+  if (firstAnimationPlayed === false) {
+    firstAnimationPlayed = true;
     $("#projects, #blog, #about").off('click');
-  } else { // turn off top items as well (and turn them on when the animation completes)
-    $("#topProjects, #topBlog, #topAbout").off('click');
+  };
+
+  //Return the bacgkround image to the activeTab following a transition
+  if (activeTab === "#topAbout") {
+    $("#about").css({"background-image":"url(about.jpg)"})
+  } else if (activeTab === "#topProjects") {
+    $("#projects").css({"background-image":"url(projects.jpg)"})
+  } else if (activeTab === "#topBlog") {
+    $("#blog").css({"background-image":"url(blog.jpg)"})
   }
 
   //passes the rest of the arguements to the animateNavigation function depending
   //on click
-  if (clickedIcon === "about"||clickedIcon === "topAbout"){
-    clickedIcon = $("#about")
+  if (clickedIcon === "#about"||clickedIcon === "#topAbout"){
+    clickedIcon = $("#about");
     otherText = $("#blogH2, #projectsH2");
     otherIcons = $("#blog, #projects");
-  } else if (clickedIcon === "blog"||clickedIcon === "topBlog") {
-    clickedIcon = $("#blog")
+    activeTab = "#topAbout"
+  } else if (clickedIcon === "#blog"||clickedIcon === "#topBlog") {
+    clickedIcon = $("#blog");
     otherText = $("#aboutH2, #projectsH2");
     otherIcons = $("#about, #projects");
-  } else if (clickedIcon === "projects"||clickedIcon === "topProjects") {
-    clickedIcon = $("#projects")
+    activeTab = "#topBlog"
+  } else if (clickedIcon === "#projects"||clickedIcon === "#topProjects") {
+    clickedIcon = $("#projects");
     otherText = $("#aboutH2, #blogH2");
     otherIcons = $("#about, #blog");
-  }
+    activeTab = "#topProjects"
+  };
+
   animateNavigation(clickedIcon, otherIcons, otherText);
 });
 
 function animateNavigation(clickedIcon, otherIcons, otherText){
   //Check if animation has already ran
-  if (navAnimationPlayed == true) {
+  if (navAnimationPlayed === true) {
     $(clickedIcon).show();
-    otherText.show()
-    otherIcons.show()
+    otherText.show();
+    otherIcons.show();
 
-    $(clickedIcon).animate({width: "100%"}, 4000)
-    otherIcons.animate({width: ""}, 4000, function(){
+    $(clickedIcon).animate({width: "100%"}, 3000)
+    otherIcons.animate({width: ""}, 3000, function(){
       otherText.hide(0);
       otherIcons.hide(0);
-
-    $("#topProjects, #topBlog, #topAbout").on('click');
-    })
+      displayText(clickedIcon);
+    });
   } else {
-  otherIcons.animate({width: ""}, 4000, function(){
-    otherText.hide(0);
-    otherIcons.hide(0);})
+    otherIcons.animate({width: ""}, 3000, function(){
+      otherText.hide(0);
+      otherIcons.hide(0);})
 
-  $(clickedIcon).animate({width: "100%"}, 4000, function(){
-    $(".navTop").fadeIn(4000)});
+  $(clickedIcon).animate({width: "100%"}, 3000, function(){
+    $(".navTop").fadeIn(2000)
+    displayText(clickedIcon);
+  });
 
-  navAnimationPlayed = true;
+  navAnimationPlayed = true; // Ensures only top loop is played from now on
   }
-activeIcon = $(clickedIcon);
+
+};
+
+
+function displayText(clickedIcon){
+  //loads html page in between Div tag on click. If no file, returns nothing
+
+  //Remove background image while page active and set opacity to none
+  $(clickedIcon).css({"background-image":"none", "opacity":"1", "text-shadow":"none"})
+  $("#" + clickedIcon[0].id + ":hover").css({"opacity":"none", "text-shadow":"none"})
+
+  if(clickedIcon[0].id === "about") {
+    $(clickedIcon).load("about.html");
+  } else if (clickedIcon[0].id === "projects") {
+    $(clickedIcon).load("projects.html");
+  } else if (clickedIcon[0].id === "blog") {
+    $(clickedIcon).load("blog.html");
+  } else {
+    return;
+  }
+
+  //TODO stop this from repeating each loop (It's redundant!)
+  $(clickedIcon).css({"overflow-y": "scroll"})
 };
