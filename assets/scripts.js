b6bc57e35347f187bcc63496c6d9d649903f6448
tdmnew <timothy.david.new@hotmail.co.uk>
2019-01-09 13:45:48 +0000

    More conversions and larger font by default

--- assets/scripts.js
+++ assets/scripts.js
@@ -28,16 +28,17 @@ function scalePictures() {
   }
 }
 
-function animateNav(nav, navHeight) {
-	//Convert units to REM
-	navHeight = navHeight / 16
+function animateNav(nav, buttons) {
+
+	// Desktop height is 2.6, or if mobile the amount of nav buttons * 2.6
+	var navHeight = ( width > 1100 ? 2.6 : buttons * 2.6);
 	var pos = 0
 	var framerate = setInterval(frame, 10)
 	function frame() {
 		if(pos > navHeight) {
 			clearInterval(framerate)
 		} else {
-			pos = pos + 0.075
+			pos = pos + 0.1
 			nav.style.height = pos + 'rem'
 		}
 	}
@@ -48,19 +49,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
 	var nav = document.getElementsByTagName('nav')[0]
 	var navBody = document.querySelector('ul')
 	let textBody = document.querySelector('.sectionChild')
-	var navHeight = ( width > 1100 ? 30 : 85)
 	var navButtons = document.getElementsByClassName("navButtons") 
 	var navButtonsCount = navBody.childElementCount
 
-	if (width < 1100 &&  navButtonsCount == 2){
-		navHeight = 57
-	}
-
 	nav.style.height = '0'
-	animateNav(nav, navHeight)
+	animateNav(nav, navButtonsCount)
 	
 	navBody.style.opacity = "0"
-
 	navBody.style.display = "none"
 
 	setTimeout(() => {
@@ -92,14 +87,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
 	}
 
     desktopSmallWidth.addListener(() => {
+		let liSize =  2.6
       if(desktopSmallWidth.matches) {
-        if(navButtonsCount == 2) {
-			nav.style.height = "3.563rem"
-		} else {
-		  nav.style.height = "5.313rem"
-		}
+		nav.style.height = (liSize * navButtonsCount + "rem")
       } else {
-       nav.style.height = "1.875rem" 
+		nav.style.height = "2.6rem" 
       }
     })
 
